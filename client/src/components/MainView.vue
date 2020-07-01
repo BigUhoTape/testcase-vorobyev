698f02d2021d5bcf9517241bd22151d20850ef8f
Ivan Vostrikov <ivanvostrikov@MacBook-Air-Ivan.local>
2020-07-01 13:58:09 +0700

    create MainView component

---
+++ client/src/components/MainView.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="mainView">
+        <h1>TodoList</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+      name: 'MainView'
+    }
+</script>
+
+<style lang="less" scoped>
+</style>
